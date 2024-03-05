d369dd2e2816ab39628dc1221f37dd7495022d33
lingesh-aradhya <79594789+lingesh-aradhya@users.noreply.github.com>
2024-03-05 10:23:22 +0530

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,27 +1,86 @@
 package test
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 	"testing"
 
+	"path/filepath"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestTerraformHelloWorldExample(t *testing.T) {
-	// Construct the terraform options with default retryable errors to handle the most common
-	// retryable errors in terraform testing.
+func TestTerraformOSDUInstaller(t *testing.T) {
+	
+	// List of environment variables to check
+	envVariables := []string{"TF_MODULE_DIR", "TF_MODULE_NAME", "TERRAFORM_ENVIRONMENT", "DIST_DIR"}
+	// Loop through the list of environment variables
+	for _, envVar := range envVariables {
+		value, exists := os.LookupEnv(envVar)
+		if exists {
+			fmt.Printf("Environment variable %s is set with value: %s\n", envVar, value)
+		} else {
+			fmt.Printf("Environment variable %s is not set\n", envVar)
+		}
+	}
+
+	tfModuleDir := os.Getenv("TF_MODULE_DIR")
+	tfModuleName := os.Getenv("TF_MODULE_NAME")
+	tfEnvironment := os.Getenv("TERRAFORM_ENVIRONMENT")
+	distDir := os.Getenv("DIST_DIR")
+	tfInitUpgrade := true
+
+	
+	tfVarFile := fmt.Sprintf("osdu/%s/%s/%s.tfvars", distDir, tfEnvironment, tfModuleName)
+	tfConfFile := fmt.Sprintf("osdu/%s/%s/%s.conf", distDir, tfEnvironment, tfModuleName)
+	tfDir := fmt.Sprintf("osdu/deployment/%s", tfModuleDir)
+
+	//terratest requires absolute patf of tfvars file
+	absolutePathTfVar, err := filepath.Abs(tfVarFile)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	tfVarFileAbsPath := []string{absolutePathTfVar}
+	
+	//Convert conf file into map of strings
+	file, err := os.Open(tfConfFile)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	tfConf := make(map[string]interface{})
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) == 2 {
+			key := parts[0]
+			value := parts[1]
+			tfConf[key] = value
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error scanning the file:", err)
+		return
+	}
+
+	//Run Terraform
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		// Set the path to the Terraform code that will be tested.
-		TerraformDir: ".",
+		TerraformDir: tfDir,
+		BackendConfig: tfConf,
+		Upgrade: tfInitUpgrade, 
+		VarFiles: tfVarFileAbsPath,
 	})
 
-	// Clean up resources with "terraform destroy" at the end of the test.
-	defer terraform.Destroy(t, terraformOptions)
+	// // Clean up resources with "terraform destroy" at the end of the test.
+	// defer terraform.Destroy(t, terraformOptions)
 
-	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Run `terraform output` to get the values of output variables and check they have the expected values.
-	output := terraform.Output(t, terraformOptions, "hello_world")
-	assert.Equal(t, "Hello, World!", output)
 }
